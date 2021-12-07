56d56940e99ec5c63aeb43852b275a517f06afdc
developerpriya <45784795+developerpriya@users.noreply.github.com>
2021-12-07 11:19:12 +0400

    Add files via upload

---
+++ api-gateway/src/test/java/com/metric/webpage/api/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metric.webpage.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
